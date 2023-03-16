2629ee2e375f468d99edec8362dfb9f9ec9d33b9
hrutik7 <62372737+hrutik7@users.noreply.github.com>
2023-03-16 14:19:25 +0530

    cors error fixed

--- src/components/currencies/Currency.jsx
+++ src/components/currencies/Currency.jsx
@@ -1,18 +1,33 @@
-import React, { useEffect } from "react";
+import React, { useEffect ,useState} from "react";
 import styles from "../../styles/currency.module.css";
 const Currency = () => {
+  const [data,setData] = useState([])
   useEffect(() => {
     cryptoCall()
-  });
+  },[]);
 
   const cryptoCall = async ()=>{
-  const res = await  fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+  const res = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=100&page=1&sparkline=false',{
+    headers: {
+      'Access-Control-Allow-Origin' : '*',
+      'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS'
+      }
+  })
     
   const result = await res.json()
 
-  console.log(result,"result")
+  setData(result)
+  console.log(result[0],"result")
   }
-  return <div className={styles.currency}>Currency</div>;
+  return (
+    <div className={styles.currency}>
+      {
+        data.map((data,key)=>{
+          console.log(key,data.id)
+        })
+      }
+    </div>
+  )
 };
 
 export default Currency;
