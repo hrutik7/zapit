64446dcc6923eb0c75c870574875fe1de05e0472
hrutik7 <62372737+hrutik7@users.noreply.github.com>
2023-03-16 17:52:49 +0530

    desktop version ui completed

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,46 @@
 import React from 'react'
+import styles from "../../styles/footer.module.css"
+import {
+  faYoutube,
+  faFacebook,
+  faTwitter,
+  faInstagram,
+  faLinkedin
+} from "@fortawesome/free-brands-svg-icons";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const Footer = () => {
   return (
-    <div>Footer</div>
+   <>
+    {/* <div className={styles.logo}>CoinWatch</div> */}
+    <div>
+      {/* <h1>Social Flow</h1> */}
+      <p className={styles.socialcontainer}>
+        
+        <a
+          href="https://www.facebook.com/"
+          className={styles.facebook}
+        >
+          <FontAwesomeIcon icon={faFacebook} size="xs" />
+        </a>
+        <a
+          href="http://www.instagram.com"
+          className={styles.instagram}
+        >
+          <FontAwesomeIcon icon={faInstagram} size="xs" />
+        </a>
+        <a
+          href="http://www.instagram.com"
+          className={styles.instagram}
+        >
+          <FontAwesomeIcon icon={faLinkedin} size="xs" />
+        </a>
+        <a href="https://wwww.twitter.com" className={styles.twitter}>
+          <FontAwesomeIcon icon={faTwitter} size="xs" />
+        </a>
+      </p>
+    </div>
+   </>
   )
 }
 
