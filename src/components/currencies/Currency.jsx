ff90758985f1625d0b4e9ee8b56f0fd103de0a7c
hrutik7 <62372737+hrutik7@users.noreply.github.com>
2023-03-16 16:50:06 +0530

    currency styled

--- src/components/currencies/Currency.jsx
+++ src/components/currencies/Currency.jsx
@@ -12,19 +12,49 @@ const Currency = () => {
       'Access-Control-Allow-Origin' : '*',
       'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS'
       }
+      ,
+      mode:'cors'
   })
     
   const result = await res.json()
 
   setData(result)
-  console.log(result[0],"result")
+  console.log(result,"result")
   }
   return (
     <div className={styles.currency}>
+      <div className={styles.currency_header}>
+        <div className={styles.srno}>
+          Sr No.
+        </div>
+        <div className={styles.Name}>
+          Name
+        </div>
+        <div className={styles.marcketcap}>
+          Marcket Cap
+        </div>
+      </div>
       {
-        data.map((data,key)=>{
-          console.log(key,data.id)
-        })
+        // console.log(data,"data")
+        data.map((data,key=>{
+         return(
+          <>
+          <div className={styles.currency_header_c} key={key}>
+        <div className={styles.srno_c}>
+          {key.market_cap_rank}
+        </div>
+        <div className={styles.Name_c}>
+        {key.name}
+        </div>
+        <div className={styles.marcketcap_c}>
+          {key.market_cap}
+        </div>
+      </div>
+
+
+          </>
+         )
+        }))
       }
     </div>
   )
