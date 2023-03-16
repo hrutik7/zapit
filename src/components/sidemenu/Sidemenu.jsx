6fece5362c2907949c13c53e96e23ed7f35a2ea0
hrutik7 <62372737+hrutik7@users.noreply.github.com>
2023-03-16 11:52:10 +0530

    sort currency type styling done

--- src/components/sidemenu/Sidemenu.jsx
+++ src/components/sidemenu/Sidemenu.jsx
@@ -4,6 +4,17 @@ const Sidemenu = () => {
   return (
     <div className={styles.sidemenu}>
       <input type="text" placeholder="search..." />
+      <nav>
+      <ul>
+      
+        <li><a>Type of currency</a>
+          <ul>
+            <li><a>Tokens</a></li>
+            <li><a>Coins</a></li>
+          </ul>
+        </li>
+      </ul>
+    </nav>
     </div>
   );
 };
