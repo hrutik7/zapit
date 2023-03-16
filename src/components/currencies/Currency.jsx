e63fb769a6eefda814f3f07f3db9f4a84060ed58
hrutik7 <62372737+hrutik7@users.noreply.github.com>
2023-03-16 13:54:55 +0530

    api called success

--- src/components/currencies/Currency.jsx
+++ src/components/currencies/Currency.jsx
@@ -1,9 +1,18 @@
-import React,{useeffect} from 'react'
-import styles from '../../styles/currency.module.css'
+import React, { useEffect } from "react";
+import styles from "../../styles/currency.module.css";
 const Currency = () => {
-  return (
-    <div className={styles.currency}>Currency</div>
-  )
-}
+  useEffect(() => {
+    cryptoCall()
+  });
 
-export default Currency
+  const cryptoCall = async ()=>{
+  const res = await  fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    
+  const result = await res.json()
+
+  console.log(result,"result")
+  }
+  return <div className={styles.currency}>Currency</div>;
+};
+
+export default Currency;
